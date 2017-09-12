3f869ad665fdb8945acb93e9617ae12a99633b0e
Carolyn Anderson <canders277@gmail.com>
2017-09-11 21:16:02 -0400

    support

--- support.py
+++ support.py
@@ -2,5 +2,18 @@ from nltk import misc
 
 words = ["regex","algorithm","semantics","modeling","cognition","neural","connectionist","parsing","syntax","tagging"]
 grid, searchList = misc.wordfinder.wordfinder(words, rows=20, cols=20, attempts=50, alph='ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-print grid
-#print searchList
+
+"""
+total = ""
+for i in grid:
+	line = ""
+	for j in i:
+		line = line+j
+	total = total +","+line
+print total
+
+"""
+words = ""
+for s in searchList:
+	words = s+","+words
+print words
