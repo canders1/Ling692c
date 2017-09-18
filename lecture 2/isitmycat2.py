7943abf6ea8d899dfd4dc838bb5fbef34179b917
Carolyn Anderson <canders277@gmail.com>
2017-09-18 09:41:05 -0400

    lecture 2 materials

--- lecture 2/isitmycat2.py
+++ lecture 2/isitmycat2.py
@@ -1,16 +1,9 @@
-name = raw_input("Please choose a cat name:\n")
-
-if (name != "Captain Haddock") and (name != "Calvin"):
-  print "That's not my cat!"
-  name2 = raw_input("Please choose another cat name:\n")
-  if (name2 != "Captain Haddock") and (name2 != "Calvin"):
-    print "That's not my cat!"
-    name3 = raw_input("Please choose another cat name:\n")
-    if(name2 != "Captain Haddock") and (name2 != "Calvin"):
+success = False
+for i in range(0,3):
+  if success == False:      #Has the user succeeded yet?
+    name = raw_input("Please choose a cat name:\n")
+    if (name != "Captain Haddock") and (name != "Calvin"):
       print "That's not my cat!"
     else:
       print "That is my cat!"
-  else:
-    print "That is my cat!"
-else:
-  print "That is my cat!"
+      success = True        #Set the value of success to True since the user has guessed correctly
--- lecture 2/isitmycat3prime.py
+++
@@ -1,7 +0,0 @@
-for i in range(0,3):
-  name = raw_input("Please choose a cat name:\n")
-  if (name != "Captain Haddock") and (name != "Calvin"):
-    print "That's not my cat!"
-  else:
-    print "That is my cat!" 
-    break
