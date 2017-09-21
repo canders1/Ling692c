7b2826b3b84427a2ac46d2b184c237f1119c9710
Carolyn Anderson <canders277@gmail.com>
2017-09-21 17:16:50 -0400

    cleaned up word scrambler

--- lecture 3/scrambler.py
+++ lecture 3/scrambler.py
@@ -3,24 +3,17 @@ import sys
 def scrambler(word):
 	scrambles = [""]
 	for i in range(0,len(word)):
-		print "iteration: ", i
 		newscrambles = []
 		for w in scrambles:
 			wList = list(w)
 			missingList = list(word)
-			print "Miss", missingList
 			for char in wList:
 				if char != "":
 					missingList.remove(char)
-			print "issing: ", missingList
-			print missingList
 			missing = set(missingList)
-			print missing
 			for j in missing:
-				print "j", j
 				newscrambles.append(w+j)
 		scrambles = newscrambles
-		print scrambles
 	return scrambles
 
 scrambles = scrambler(raw_input("Please give me a word: "))
