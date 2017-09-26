8ce7c0b19856a899afb22b294190f9156b532704
Carolyn Anderson <canders277@gmail.com>
2017-09-25 20:18:01 -0400

    comments

--- lecture 3/pyramid.py
+++ lecture 3/pyramid.py
@@ -1,20 +1,34 @@
+"""
+Author: Carolyn Anderson
+Date: 9/25/17
+
+Prints out a pyramid of characters specified by the user.
+
+Input: char: character to be printed
+		height: number of lines in pyramid
+		which: left-slanted or centered pyramid
+"""
+
 import sys
 
-char = sys.argv[1]
-height = int(sys.argv[2])
-which = sys.argv[3]
+def main():
 
-if which == "0":
-	start = char
-	print start
-	for i in range(1,height):
-		start = start+char+char
+	char = sys.argv[1]
+	height = int(sys.argv[2])
+	which = sys.argv[3]
+
+	if which == "0": #Left-leaning pyramid
+		start = char
 		print start
-else:
-	start = char
-	for i in range(1,height):
-		start = " "+start
-	print start
-	for i in range(1,height):
-		start = start[1:]+char+char
+		for i in range(1,height):
+			start = start+char+char
+			print start
+	else: #Centered pyramid
+		start = char
+		for i in range(1,height):
+			start = " "+start
 		print start
+		for i in range(1,height):
+			start = start[1:]+char+char
+			print start
+main()
