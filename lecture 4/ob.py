d29efca00c5997e93f47051df70fceb62fc1adfa
Carolyn Anderson <canders277@gmail.com>
2017-10-01 13:27:54 -0400

    move anagram stuff to lecture 5

---
+++ lecture 4/ob.py
@@ -0,0 +1,14 @@
+def oo000 ( ) :
+ ii = 12
+ for ii in range ( 0 , ii ) :
+  print oOOo ( ii )
+  if 59 - 59: Oo0Ooo . OO0OO0O0O0 * iiiIIii1IIi . iII111iiiii11 % I1IiiI
+def oOOo ( n ) :
+ if n <= 1 :
+  if 27 - 27: iIiiiI1IiI1I1 * IIiIiII11i * IiIIi1I1Iiii - Ooo00oOo00o
+  if 48 - 48: oO0o / OOooOOo / I11i / Ii1I
+  return n
+ else :
+  return ( oOOo ( n - 1 ) + oOOo ( n - 2 ) )
+  if 48 - 48: iII111i % IiII + I1Ii111 / ooOoO0o * o00O0oo
+oo000 ( ) 
