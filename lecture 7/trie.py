1d73d00c7aab5f387cd6f14d827d50d24e7d1808
Carolyn Anderson <canders277@gmail.com>
2017-10-30 12:13:41 -0400

    trie

--- lecture 7/trie.py
+++ lecture 7/trie.py
@@ -15,12 +15,13 @@ class Node:
 
 class Trie:
 	def __init__(self,vals):
-		self.root = Node("")
+		self.val = ""
+		self.children = None
 		for v in vals:
 			self.insert(v)
 
 	def insert(self,val):
-		curr = self.root
+		curr = self
 		for i in range(0,len(val)):
 			newcurr = Node(val[i:i+1])
 			if curr.children == None:
@@ -36,11 +37,34 @@ class Trie:
 					curr.children.append(newcurr)
 			curr = newcurr
 
+	def getValues(self):
+		v = []
+		for c in self.children:
+			v.append(c.val)
+		return v
+
+	def get(self,val):
+		curr = self
+		for i in range(0,len(val)+1):
+			if i == len(val):
+				return curr
+			if curr.children==None:
+				return False
+			else:
+				missing = True
+				for c in curr.children:
+					if c.val == val[i:i+1]:
+						missing = False
+						curr = c
+						break
+					if missing:
+						return False
+
 
 t = Trie(["cat","cats","cast","cab"])
 
-print t.root.children[0].val
-for v in t.root.children[0].children[0].children:
-	print v.val
-					
+#print t.root.children[0].val
+#for v in t.root.children[0].children[0].children:
+	#print v.val
+print t.get("cats").val
 
