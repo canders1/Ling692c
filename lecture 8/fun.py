9fe8e9c47ea2f77ef21fae1201872400c3d23581
Carolyn Anderson <canders277@gmail.com>
2017-11-07 07:00:13 -0500

    fun

---
+++ lecture 8/fun.py
@@ -0,0 +1,10 @@
+from nltk import misc
+
+misc.chomsky.generate_chomsky(times=5, line_length=72)
+
+
+words = ["CHOMSKY","SYNTAX","SEMANTICS","PHONEME","LEXICON"]
+words, puzzle = misc.wordfinder.wordfinder(words, rows=10, cols=10, attempts=50, alph='ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+for w in words:
+	print " ".join(w)
+print puzzle
